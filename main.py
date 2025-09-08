f9d263dc4676c4355abc83f95b43c41b1e26594d
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-09-09 00:26:44 +0500

    добавлен main.py с регистрацией всех хендлеров

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import telebot
 from config import TOKEN
 
+
+# подключаем хендлеры
+from handlers.start import register_start_handler
+from handlers.staff import register_staff_handlers
+from handlers.menu import register_menu_handlers
+
 bot = telebot.TeleBot(TOKEN)
+
+# регистрируем хендлеры
+register_start_handler(bot)
+register_staff_handlers(bot)
+register_menu_handlers(bot)
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
